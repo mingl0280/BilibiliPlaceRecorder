e8d00cf2741b36e224af6ec41b2d494cb2ab1898
pmx <mingl0280@gmail.com>
2017-08-13 17:57:55 -0600

    init commit
    Signed-off-by: pmx <mingl0280@gmail.com>

--- WeirdReq/Form1.Designer.vb
+++ WeirdReq/Form1.Designer.vb
@@ -1,9 +1,10 @@
-﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+﻿Imports Gecko
+<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
 Partial Class Form1
     Inherits System.Windows.Forms.Form
 
     'Form 重写 Dispose，以清理组件列表。
-    <System.Diagnostics.DebuggerNonUserCode()> _
+    <System.Diagnostics.DebuggerNonUserCode()>
     Protected Overrides Sub Dispose(ByVal disposing As Boolean)
         Try
             If disposing AndAlso components IsNot Nothing Then
@@ -14,17 +15,46 @@ Partial Class Form1
         End Try
     End Sub
 
+
+
     'Windows 窗体设计器所必需的
     Private components As System.ComponentModel.IContainer
 
     '注意: 以下过程是 Windows 窗体设计器所必需的
     '可以使用 Windows 窗体设计器修改它。  
     '不要使用代码编辑器修改它。
-    <System.Diagnostics.DebuggerStepThrough()> _
+    <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
-        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.Text = "Form1"
-    End Sub
+        Me.components = New System.ComponentModel.Container()
+        Me.gWebBrowser = New Gecko.GeckoWebBrowser()
 
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.SuspendLayout()
+        '
+        'gWebBrowser
+        '
+        Me.gWebBrowser.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.gWebBrowser.FrameEventsPropagateToMainWindow = False
+        Me.gWebBrowser.Location = New System.Drawing.Point(0, 0)
+        Me.gWebBrowser.Name = "gWebBrowser"
+        Me.gWebBrowser.Size = New System.Drawing.Size(1853, 1023)
+        Me.gWebBrowser.TabIndex = 0
+        Me.gWebBrowser.UseHttpActivityObserver = False
+        '
+        'Timer1
+        '
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1853, 1023)
+        Me.Controls.Add(Me.gWebBrowser)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents gWebBrowser As GeckoWebBrowser
+    Friend WithEvents Timer1 As Timer
 End Class
--- WeirdReq/Form1.vb
+++ WeirdReq/Form1.vb
@@ -1,3 +1,38 @@
-﻿Public Class Form1
+﻿Imports Gecko.DOM
+Imports Gecko.Events
+Imports System.IO
+Imports System.Drawing.Imaging
+
+Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        gWebBrowser.Navigate("http://live.bilibili.com/pages/1702/pixel-drawing")
+    End Sub
+
+    Dim refCounter As Integer = 0
+
+    Private Sub gWebBrowser_DocumentCompleted(sender As Object, e As GeckoDocumentCompletedEventArgs) Handles gWebBrowser.DocumentCompleted
+        Timer1.Interval = 10 * 1000
+        If Not Directory.Exists("CanvasSnippets") Then
+            Directory.CreateDirectory("CanvasSnippets")
+        End If
+        Timer1.Start()
+        Timer1_Tick(New Object, New EventArgs)
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        Dim obj As GeckoCanvasElement = gWebBrowser.Document.GetElementById("drawing-canvas")
+        Dim DataStr As String = obj.ToDataURL("image/png")
+        Dim ImageBytes() As Byte = Convert.FromBase64String(DataStr.Substring("data:image/png;base64,".Length))
+        Dim Image As Image = Image.FromStream(New MemoryStream(ImageBytes))
+        Image.Save("CanvasSnippets\" + getNowTimestamp() + ".png", ImageFormat.Png)
+        refCounter = refCounter + 1
+        If refCounter Mod 100 = 0 Then
+            gWebBrowser.Reload()
+        End If
+    End Sub
+
+    Private Function getNowTimestamp() As String
+        Return Convert.ToInt64(DateTime.UtcNow.Subtract(New DateTime(1970, 1, 1)).TotalMilliseconds()).ToString()
+    End Function
 
 End Class
